e6abbedd4e0aa985b4642ad0aef32c1c037bb086
Brandon Kelton <brandon.kelton@outlook.com>
2019-04-14 08:33:04 -0600

    Add project files.

---
+++ InteractiveClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InteractiveClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
